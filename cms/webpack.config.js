c7abe7496b5a920f8cd5ea6408371748eb203881
NotHoudaifa <houdaifacherifi618@gmail.com>
2022-10-30 20:36:31 +0000

    Setted up the cms

---
+++ cms/webpack.config.js
@@ -0,0 +1,54 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+const mode = "development";
+
+module.exports = {
+    mode: mode,
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './build'),
+        filename: '[name].[contenthash].js',
+        clean: true,
+    },
+    devtool: mode === "development" ? "eval-source-map" : false,
+    devServer: {
+        static: path.resolve(__dirname, './build'),
+        port: '4000',
+        open: true,
+        hot: true,
+        historyApiFallback: true,
+    },
+    resolve: {
+        extensions: ['.js', '.jsx', '.json'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ["style-loader", 'css-loader'],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                include: path.resolve(__dirname, 'src'),
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: path.resolve(__dirname, 'public/index.html'),
+            // favicon: path.resolve(__dirname, 'public/favicon.ico'),
+        })
+    ],
+};
